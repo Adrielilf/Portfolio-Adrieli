36b3da6bdf9e75a22f45a543c7f0002fd7ab8671
Adrieli Lavratti <adritti@hotmail.com>
2022-09-02 21:05:30 -0300

    background/estrutura de pastas/assets

---
+++ portifolio-adrieli/src/App.js
@@ -0,0 +1,14 @@
+import { GlobalStyled} from './Globalstyled';
+import Headers from './components/header/header';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyled/>
+      <Headers/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ portifolio-adrieli/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ContainerHeader,Nav} from './styled';
+
+const Headers = () => {
+    return (
+        <ContainerHeader>   
+                         
+        </ContainerHeader>
+    );
+};
+export default Headers;
